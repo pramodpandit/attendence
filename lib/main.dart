15527950aa88a2071a21a4cc1e274d251f628886
Pramod Pandit <pramod@aarvytechnologies.co.in>
2024-03-21 22:27:31 -0700

    22-03-2024

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ void main() async {
           enableVibration: true,
           onlyAlertOnce: true,
         ),
-      ]);
+      ],);
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   final sharedPreferences = await SharedPreferences.getInstance();
@@ -110,7 +110,7 @@ void main() async {
 }
 
 // Declared as global, outside of any class
-@pragma('vm:entry-point')
+@pragma("vm:entry-point")
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
@@ -122,29 +122,43 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   //         considerWhiteSpaceAsEmpty: true) ||
   //     !AwesomeStringUtils.isNullOrEmpty(message.notification?.body,
   //         considerWhiteSpaceAsEmpty: true)) {
-  //   print('message also contained a notification: ${message.notification}');
-  //
-  //   String? imageUrl;
-  //   imageUrl ??= message.notification!.android?.imageUrl;
-  //   imageUrl ??= message.notification!.apple?.imageUrl;
-  //
-  //   Map<String, dynamic> notificationAdapter = {
-  //     NOTIFICATION_CHANNEL_KEY: 'basic_channel',
-  //     NOTIFICATION_ID: message.data[NOTIFICATION_CONTENT]?[NOTIFICATION_ID] ??
-  //         message.messageId ??
-  //         Random().nextInt(2147483647),
-  //     NOTIFICATION_TITLE: message.data[NOTIFICATION_CONTENT]
-  //             ?[NOTIFICATION_TITLE] ??
-  //         message.notification?.title,
-  //     NOTIFICATION_BODY: message.data[NOTIFICATION_CONTENT]
-  //             ?[NOTIFICATION_BODY] ??
-  //         message.notification?.body,
-  //     NOTIFICATION_LAYOUT:
-  //         AwesomeStringUtils.isNullOrEmpty(imageUrl) ? 'Default' : 'BigPicture',
-  //     NOTIFICATION_BIG_PICTURE: imageUrl
-  //   };
-  //
-  //   AwesomeNotifications().createNotificationFromJsonData(notificationAdapter);
+    print('message also contained a notification: ${message.notification}');
+
+    String? imageUrl;
+    imageUrl ??= message.notification!.android?.imageUrl;
+    imageUrl ??= message.notification!.apple?.imageUrl;
+
+    Map<String, dynamic> notificationAdapter = {
+      NOTIFICATION_CONTENT : {
+        NOTIFICATION_CHANNEL_KEY: 'basic_channel',
+        NOTIFICATION_ID: message.data[NOTIFICATION_CONTENT]?[NOTIFICATION_ID] ??
+            message.messageId ??
+            Random().nextInt(2147483647),
+        NOTIFICATION_TITLE: message.data[NOTIFICATION_CONTENT]
+        ?[NOTIFICATION_TITLE] ??
+            message.notification?.title,
+        NOTIFICATION_BODY: message.data[NOTIFICATION_CONTENT]
+        ?[NOTIFICATION_BODY] ??
+            message.notification?.body,
+        NOTIFICATION_LAYOUT:
+        AwesomeStringUtils.isNullOrEmpty(imageUrl) ? 'Default' : 'BigPicture',
+        NOTIFICATION_BIG_PICTURE: imageUrl,
+      },
+      NOTIFICATION_ACTION_BUTTONS : [
+        {
+          NOTIFICATION_BUTTON_KEY : "ACCEPT",
+          NOTIFICATION_BUTTON_LABEL : "Accept",
+          NOTIFICATION_ENABLED : true,
+        },
+        {
+          NOTIFICATION_BUTTON_KEY : "REJECT",
+          NOTIFICATION_BUTTON_LABEL : "Reject",
+          NOTIFICATION_ENABLED : true,
+        }
+      ]
+    };
+
+    AwesomeNotifications().createNotificationFromJsonData(notificationAdapter);
   // } else {
   //   AwesomeNotifications().createNotificationFromJsonData(message.data);
   // }
